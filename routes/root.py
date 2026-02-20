07eab3e6fd9dfdd0eb77af74fe5ec55485838b0f
Jamie Whitehair <jamieabw@protonmail.com>
2026-02-20 18:15:24 +0000

    Added ability to create/join group

--- routes/root.py
+++ routes/root.py
@@ -1,8 +1,41 @@
-from flask import Blueprint, render_template, redirect, url_for
-from flask_login import login_required
+from flask import Blueprint, render_template, redirect, url_for, request
+from flask_login import login_required, current_user
+from werkzeug import security
+from configuration import db
+from models import Groups, GroupMembers
 rootBp = Blueprint("root", __name__, url_prefix="")
 
 @rootBp.route("/")
 @login_required
 def index():
-    return render_template("index.html")
+    usersGroups = []
+    for group in GroupMembers.query.filter_by(userId=current_user.id):
+        print(Groups.query.filter_by(id=group.id).first().name) # temp
+        usersGroups.append(Groups.query.filter_by(id=group.id).first())
+    return render_template("index.html", groups=usersGroups)
+
+# these two group creation and join routes are temporary, they will be replaced with a pop up form.
+@rootBp.route("/createGroup", methods=["GET", "POST"])
+@login_required
+def createGroup():
+    if request.method == "GET":
+        return render_template("createGroupTemp.html")
+    else:
+        try:
+            groupName = request.form["groupName"]
+            groupPassword = security.generate_password_hash(request.form["groupPassword"])
+            db.session.add(Groups(groupName, groupPassword))
+
+            db.session.add(GroupMembers(current_user.id, Groups.query.filter_by(name=groupName).first().id))
+            db.session.commit()
+            print("group added!")
+            return redirect(url_for(".index"))
+        except Exception:
+            db.session.rollback()
+            return render_template("createGroupTemp.html")
+
+
+@rootBp.route("/joinGroup")
+@login_required
+def joinGroup():
+    return render_template("joinGroupTemp.html")
